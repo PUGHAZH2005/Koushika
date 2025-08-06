e5f5117d384364a0d595f5d9e0ad0f97a3a97659
GOKUL L <jaganyrr4546@gmail.com>
2025-08-06 15:46:31 +0530

    Update gunicorn_config.py

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,13 +1,10 @@
 # gunicorn_config.py
 
 # Number of worker processes
-workers = 1
+workers = 1 
 
 # The socket to bind to
 bind = '0.0.0.0:10000'
 
-# Timeout for waiting for workers to boot (in seconds).
-# Also the timeout for handling individual requests.
-# We increase this from the default (30s) to give Matplotlib
-# time to build its font cache on the first run.
+# The timeout for workers
 timeout = 120

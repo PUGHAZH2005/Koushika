5744027989f492c93549ea1bf9704cb985209932
GOKUL L <jaganyrr4546@gmail.com>
2025-08-06 16:22:09 +0530

    Update gunicorn_config.py

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,10 +1,17 @@
 # gunicorn_config.py
 
-# Number of worker processes
+# --- FIX: Define all settings here for reliability ---
+
+# Set the worker class to the efficient gevent worker
+worker_class = 'gevent'
+
+# Set the number of worker processes
 workers = 1 
 
-# The socket to bind to
-bind = '0.0.0.0:10000'
+# The socket to bind to. Render provides the PORT environment variable.
+# We use a default of 10000 for local testing if the variable isn't set.
+port = os.environ.get('PORT', '10000')
+bind = f'0.0.0.0:{port}'
 
 # The timeout for workers
 timeout = 120
